c9c6dec82c78d5eb3ebe94ee1a5bb9459de680cb
jasonaruiz95 <jasonruiz1@mail.weber.edu>
2023-04-16 09:29:15 -0600

    Initial commit. Login and registration are working, landing page has been developed

---
+++ transcriptionSite/users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
+from django.contrib import messages
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}! You are logged in!')
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
